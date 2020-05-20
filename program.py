ee890ff8ca3e1ec79324dd3679a21a9fcfbf67f7
yovan <yovanjulioadam@gmail.com>
2020-05-20 18:03:59 +0700

    first

---
+++ program.py
@@ -0,0 +1,16 @@
+from tkinter import *
+import os
+os.system('clear')
+
+#############################
+
+
+root = Tk()
+root.title('Covid-19 Tracker ~ Program by Yovan')
+root.geometry('800x600')
+
+
+
+
+
+root.mainloop()
