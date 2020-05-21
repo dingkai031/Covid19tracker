4676f638ce12144106a718414f76d07202c6dc0b
yovan <yovanjulioadam@gmail.com>
2020-05-21 17:25:08 +0700

    menu selesai

--- program.py
+++ program.py
@@ -1,7 +1,4 @@
 from tkinter import *
-import os
-os.system('clear')
-
 #############################
 
 
@@ -9,8 +6,22 @@ root = Tk()
 root.title('Covid-19 Tracker ~ Program by Yovan')
 root.geometry('800x600')
 
+def donothing():
+   filewin = Toplevel(root)
+   button = Button(filewin, text="Do nothing button")
+   button.pack()
 
 
+button = Button(root, text = "testing")
+button.pack()
 
+menubar = Menu(root)
+
+helpmenu = Menu(menubar, tearoff=0)
+menubar.add_cascade(label="Help", menu=helpmenu)
+helpmenu.add_command(label="Tentang", command=donothing)
+helpmenu.add_command(label="Keluar", command=root.quit)
+
+root.config(menu=menubar)
 
 root.mainloop()
